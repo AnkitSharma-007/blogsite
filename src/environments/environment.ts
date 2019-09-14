c862c8ab93fbd09adac2ea005b455ed5d1ebf4bf
Ankit <33789321+AnkitSharma-007@users.noreply.github.com>
2019-09-14 17:54:06 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDIIXP-aChvBwutRNV8Rmf2a2AAMR71tlA",
+    apiKey: "AIzaSyDIIXP-aChvBwuV8RmfusAAMR71tlA",
     authDomain: "ngblog-4fd08.firebaseapp.com",
     databaseURL: "https://ngblog-4fd08.firebaseio.com",
     projectId: "ngblog-4fd08",
     storageBucket: "",
     messagingSenderId: "1091579244923",
-    appId: "1:1091579244923:web:62e0c7ba2df5df73158dde"
+    appId: "1:1091563244923:web:62e0c7ba2df5df73158dde"
   }
 };
 
